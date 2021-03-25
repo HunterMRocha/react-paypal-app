4084df0d6799e38c66712f455ec62caeb686a09f
HunterMRocha <huntermacias20@gmail.com>
2021-03-25 04:49:50 -0500

    implemented paypal auth 🤑

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './App.css'
+import NFT from './assets/nft1.jpg'
+import PaypalButton from "./components/PaypalButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  state = {
+    showPaypal: false
+  };
+
+  showPaypalButtons = () => {
+    this.setState({ showPaypal: true });
+  };
+
+  render() {
+    const { showPaypal } = this.state;
+    if (showPaypal) {
+      return <PaypalButton />;
+    } else {
+      return (
+        <div className="App-header">
+          <h2> LIMITED EDITION NFT </h2>
+          <img width="550" height="300" alt="nft-image" src={NFT} />
+          <h3>
+            <b>PRICE $2000</b>
+          </h3>
+          <button className='button1' onClick={this.showPaypalButtons}> Pay </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
